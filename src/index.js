a34b8d53c292498be12662c451e26f118b5a00be
Carlos Melendez <carlosmelendez@Carloss-MacBook-Pro.local>
2019-06-06 10:14:08 -0400

    Hello World

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const getCurrentDate = () => {
+    const date = new Date();
+    return date.toDateString();
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const greeting = <h1>Hello World! Current Date: {getCurrentDate()}</h1>
+
+ReactDOM.render(greeting, document.getElementById('root'));
+//registerServiceWorker();
