7041c6858196866523d234bdb9e00ab6d79b7785
Carlos Melendez <carlosmelendez@Carloss-MacBook-Pro.local>
2019-06-06 11:23:27 -0400

    Moved folders and created files and name changes

---
+++ src/components/App/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Intro from '../Intro';
+
+class App extends Component {
+  state = {
+    series: []
+  }
+  
+  render() {
+    return (
+      <div className="App">
+       <header className="App-header">
+         <h1 className="App-title">TV Series List</h1>
+       </header>
+      <Intro message='Here you can find all of your most loved series' />
+      The length of series array - {this.state.series.length}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 //import registerServiceWorker from './registerServiceWorker';
 
-const getCurrentDate = () => {
-    const date = new Date();
-    return date.toDateString();
-}
 
-const greeting = <h1>Hello World! Current Date: {getCurrentDate()}</h1>
-
-ReactDOM.render(greeting, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 //registerServiceWorker();
