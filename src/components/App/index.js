69b986f46fbf1d877c046395a65507beefc45df1
Carlos Melendez <carlosmelendez@Carloss-MacBook-Pro.local>
2019-06-06 13:43:37 -0400

    created series array

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Intro from '../Intro';
+import Series from '../../containers/series'
+import 'whatwg-fetch';
+
 
 class App extends Component {
-  state = {
-    series: []
-  }
+  
   
   render() {
     return (
@@ -14,7 +15,7 @@ class App extends Component {
          <h1 className="App-title">TV Series List</h1>
        </header>
       <Intro message='Here you can find all of your most loved series' />
-      The length of series array - {this.state.series.length}
+      <Series />
       </div>
     );
   }
